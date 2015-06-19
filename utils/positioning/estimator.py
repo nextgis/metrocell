b0083d3f40793e5efc58dc82593a246116a5ddff
sasfeat <sasfeat@mail.ru>
2015-06-20 00:25:45 +0300

    "by LacCidMod" algorithm attached, estimaton scripts improved

--- utils/positioning/estimator.py
+++ utils/positioning/estimator.py
@@ -8,33 +8,114 @@ import numpy as np
 from posAlgorithm import PosAlgorithm
 
 class Estimator():
+    def __init__(self,iters):
+        # iteration limit for each algorithm. After finish find the mean of maximum error.
+        self.iters = iters
+    def main(self,outputs):
+        """
+        Initialize algorithms of estimation.
+        :param outputs: which algorithms need to execute
+        :return: prints several numbers for each algorithm
+        """
+        for out in outputs:
+            # list of errors.Error - distance delta from the truth point
+            self.errors = []
+            # the number of true and false segments:
+            # True segment is the segment that contains user's position
+            # False segment is the segment that do not contains user's position
+            self.trueSegs,self.falseSegs = 0,0
+            if out == "random":
+                self.random()
+            if out == "byLacCid":
+                self.byLaccid()
+            if out == "byLacCidMod":
+                # the number of unpredicted segments.
+                # Unpredicted segment is the segment that do not have full set of laccids,
+                # grabbed by user into this moment. That means, that database is not full.
+                self.unpredicted=0
+                self.byLacCidMod()
+        return
+    def random(self):
+        """
+        The simplest - grab random points
+        :return:
+        """
+        print("Mean maxima-errors(meters):")
+        print("--Random--")
+        for i in range(1,self.iters):
+            randomAlg = PosAlgorithm(type = "lc")
+            randomAlg.randomSampling(randomAlg.SmoothedDf)
+            #error
+            error,predictedSeg= Estimator.byLatLong(randomAlg.predicted_df,
+                                                    randomAlg.truthPoint)
+            self.errors.append(error)
+
+        print (np.mean(self.errors))
+    def byLaccid(self):
+        """
+        Use Lac and Cid identifiers of Base station only.
+        :return:
+        """
+        for i in range(0,self.iters):
+            byLaccid = PosAlgorithm(type = "lc")
+            byLaccid.byLacCid()
+            #error
+            error,predictedSeg = Estimator.byLatLong(byLaccid.predicted_df,
+                                                     byLaccid.truthPoint,
+                                                     byLaccid.predicted_segments)
+            self.errors.append(error)
+            self.trueSegs+=predictedSeg['trueSeg']
+            self.falseSegs+=predictedSeg['falseSeg']
+        print("--By LACCID--")
+        print (np.mean(self.errors))
+        print ("false segments:" + str(self.falseSegs))
+        print ("true segments:" + str(self.trueSegs))
+    def byLacCidMod(self):
+        """
+        Use the information from neighbours laccids.
+        :return:
+        """
+
+        for i in range(0,self.iters):
+            byLaccidMod = PosAlgorithm(type = "lc")
+            byLaccidMod.byLacCidMod()
+            self.unpredicted += byLaccidMod.unpredicted
+            #error
+            error,predictedSeg = Estimator.byLatLong(byLaccidMod.predicted_df,
+                                                     byLaccidMod.truthPoint,
+                                                     byLaccidMod.predicted_segments)
+            self.errors.append(error)
+
+            self.trueSegs+=predictedSeg['trueSeg']
+            self.falseSegs+=predictedSeg['falseSeg']
+        # percent of unpredicted segments
+        unpr_per = self.unpredicted/self.iters
+        print("--By LACCID Modified--")
+        print (np.mean(self.errors))
+        print ("Persent of unpredicted segments:")
+        print (str(unpr_per)+"%")
+        print ("false segments:" + str(self.falseSegs))
+        print ("true segments:" + str(self.trueSegs))
     @staticmethod
-    def byLatLong(predictedDf,truthPoint):
+    def byLatLong(predictedDf,truthPoint,truthSegments = None):
+        predictedSegs = {'trueSeg':0,'falseSeg':999999}
+        if truthSegments!=None:
+            if truthPoint[truthPoint['segment'].isin(truthSegments)].shape[0]==1:
+                predictedSegs['trueSeg'] = 1
+            predictedSegs['falseSeg'] = len(truthSegments)-1
         test = pd.DataFrame()
         truth = zip(truthPoint['x'],truthPoint['y'])
+
         test['xy'] = zip(predictedDf['x'],predictedDf['y'])
         distances = test.apply(lambda df:vincenty(truth,df['xy']).meters,axis = 1)
         error = max(distances)
-        return error
+        error = float("%.1f"%error)
+        return error,predictedSegs
 if __name__ =="__main__":
-    #Algorithm 1 : Random
+    #parameters
+    outputs = ["random","byLacCid","byLacCidMod"]
     iters = 50
-    errors = []
-    for i in range(1,iters):
-        randomAlg = PosAlgorithm()
-        randomAlg.randomSampling(randomAlg.SmoothedDf)
-        #error
-        error= Estimator.byLatLong(randomAlg.predicted_df,randomAlg.truthPoint)
-        errors.append(error)
-    print("###Random###")
-    print ("Mean maxima-error is " + str(np.mean(errors)) + " meters")
-    #Algorithm 2: By LACCID
-    errors = []
-    for i in range(1,iters):
-        byLaccid = PosAlgorithm()
-        byLaccid.byLacCid()
-        #error
-        error = Estimator.byLatLong(byLaccid.predicted_df,byLaccid.truthPoint)
-        errors.append(error)
-    print("###By LACCID###")
-    print ("Mean maxima-error is " + str(np.mean(errors)) + " meters")
+    #instance
+    estimator = Estimator(iters = iters)
+    #initialize estimation algorithms
+    estimator.main(outputs)
--- utils/positioning/posAlgorithm.py
+++ utils/positioning/posAlgorithm.py
@@ -3,20 +3,105 @@ __author__ = 'Alex'
 import pandas as pd
 import paths
 import random
+import itertools
+    
 from utils import Utils
+
 class PosAlgorithm():
-    def __init__(self):
+    def __init__(self,type = "random"):
+        self.ut = Utils()
+        # original database
         self.SmoothedDf = pd.io.parsers.read_csv(paths.saveCellSmoothed)
         self.predicted_df = None
-        self.grabbedDf = self.randomSampling(self.SmoothedDf,numsamples = 1)
+        # by default the number of unpredicted segments is 0
+        self.unpredicted = 0
+        # The number of laccids,grabbed by user. For "byLacCidMod" algorithm it must be more then 2.
+        # Otherwise, it will works as "byLacCid" algorithm
+        self.numLC = 4
+        # initialize self.segments variable
+        self.generateRandomSegment()
+        self.grabbedDf = self.initAlg(type = type)
         self.truthPoint = self.randomSampling(self.grabbedDf,numsamples = 1)
-        self.ut = Utils()
+    def initAlg(self,type = "random"):
+        """
+        initialize the algorithm of positioning.
+        :param type: the type of dataframe grabbed by user.
+        "lc" - using laccids info.
+        "random" - randomly
+        :return:
+        """
+        if type == "random":
+            return self.randomSampling(self.SmoothedDf,numsamples = 50)
+        if type == "lc":
+            return self.getTests()
+    def generateRandomSegment(self):
+        """
+        Generate segment where user located.
+        :return:
+        """
+        uniqueLc = self.SmoothedDf.groupby(['segment'])['laccid'].unique()
+        # get rows with number of laccids more than ...
+        suits = uniqueLc[uniqueLc.apply(len)>=self.numLC]
+        segments = list(suits.keys())
+        self.randSeg = random.sample(segments,1)
 
+
+    def getTests(self):
+        """
+        Get the dataframe grabbed by user.
+        :return:
+        """
+        df = self.SmoothedDf [self.SmoothedDf['segment'].isin(self.randSeg)]
+        lc_list = self.ut.unique(df,'laccid')
+        df = df[df['laccid'].isin(random.sample(lc_list,self.numLC-1))]
+        return df
     def randomSampling(self,df,numsamples = 50):
+        """
+        Generate subset from input dataframe.
+        :param df: dataframe to analyse
+        :param numsamples: the number of samples
+        :return:
+        """
         rows = random.sample(df.index,numsamples)
         self.predicted_df = df.ix[rows]
+        self.predicted_segments = self.ut.unique(self.predicted_df,'segment')
         return self.predicted_df
     def byLacCid(self):
-        gr_laccids =  self.ut.unique(self.grabbedDf,'laccid')
-        self.predicted_df = self.SmoothedDf[self.SmoothedDf['laccid'].isin(gr_laccids)]
-        #segments = self.ut.unique(self.predicted_df,'segment')
+        """
+        Use Lac and Cid identifiers of Base station only.
+        :return:
+        """
+        self.grabbed_lc =  self.ut.unique(self.grabbedDf,'laccid')
+        self.predicted_df = self.SmoothedDf[self.SmoothedDf['laccid'].isin(self.grabbed_lc)]
+        self.predicted_segments = self.ut.unique(self.predicted_df,'segment')
+    def byLacCidMod(self):
+        """
+        Use the information from neighbours laccids.
+        :return:
+        """
+        #Note! Attach probability according  length of founded laccids at each step.
+        # For example,probability for sublist with length 4 more than siblist with length 2,
+        # because this means that in the first case 4 cell's stations were founded correctly, when
+        # in the second case only 2. But it might be lack of the data in origin database.
+        predicted_segments =[]
+        # get predicted frame and segments according base laccid algorithm
+        self.byLacCid()
+        # iterate by laccids into grabbed list of laccids.
+        for step in range(len(self.grabbed_lc),2,-1):
+            # check all combinations
+            for sublist in itertools.combinations(self.grabbed_lc,step):
+                predicted_subDf = self.predicted_df[self.predicted_df['laccid'].isin(sublist)]
+                segments = self.ut.unique(predicted_subDf,'segment')
+                # find the right segments for this combination
+                for seg in segments:
+                    seg_subDf = predicted_subDf[predicted_subDf['segment'] == seg]
+                    lc_subList = self.ut.unique(seg_subDf,'laccid')
+                    if (set(sublist).issubset(set(lc_subList))) == True:
+                        predicted_segments.append(seg)
+        # if something founded - reduce the selection of predicted segments.
+        if predicted_segments!=[]:
+            self.predicted_segments = predicted_segments
+            self.predicted_df = self.predicted_df[self.predicted_df['segment'].isin(self.predicted_segments)]
+        # if no segments - use the segments from base algorithm.
+        else:
+            self.unpredicted = 1
